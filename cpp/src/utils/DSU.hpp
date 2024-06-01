0c5f2f8c0d400cf802ea6dc938bbc469099e3907
William Yue <williamy330@gmail.com>
2024-06-01 19:04:23 -0400

    remove union by size and allow smaller dsu memory footprint

--- cpp/src/utils/DSU.hpp
+++ cpp/src/utils/DSU.hpp
@@ -9,32 +9,37 @@
 */
 
 #include <array>
+#include <type_traits>
 
 namespace SPRL {
 
 /**
- * Disjoint set union with path compression and union by size.
+ * Disjoint set union with path compression.
+ * 
+ * @tparam T The type of the indices in the DSU, e.g. `uint8_t`, etc.
+ *           Should be able to hold the range `[0, N]`.
  * 
  * @tparam N The number of elements in the DSU.
 */
-template <int N>
+template <typename T, int N>
 class DSU {
 public:
+    static_assert(N > 0, "DSU size must be greater than 0.");
+    static_assert(std::is_integral_v<T>, "DSU index type must be integral.");
+
     /**
      * Constructs a DSU object and initializes the parent and size arrays.
     */
     DSU() {
-        for (int i = 0; i < N; ++i) {
+        for (T i = 0; i < static_cast<T>(N); ++i) {
             m_parent[i] = i;
         }
-
-        m_size.fill(1);
     }
 
     /**
      * @returns The representative of the set containing `x`.
     */
-    int find(int x) const {
+    T find(T x) const {
         if (m_parent[x] != x) {
             // Path compression
             m_parent[x] = find(m_parent[x]);
@@ -43,42 +48,26 @@ public:
     }
 
     /**
-     * Unites the sets containing `x` and `y`.
+     * Unites the sets containing `x` into the set containing `y`.
     */
-    void unite(int x, int y) {
-        int root_x = find(x);
-        int root_y = find(y);
+    void unite(T x, T y) {
+        T root_x = find(x);
+        T root_y = find(y);
 
         if (root_x == root_y) return;
 
-        // Union by size
-        if (m_size[root_x] < m_size[root_y]) {
-            m_parent[root_x] = root_y;
-            m_size[root_y] += m_size[root_x];
-
-        } else {
-            m_parent[root_y] = root_x;
-            m_size[root_x] += m_size[root_y];
-        }
+        m_parent[root_x] = root_y;
     }
 
     /**
      * @returns Whether `x` and `y` are in the same set.
     */
-    bool sameSet(int x, int y) const {
+    bool sameSet(T x, T y) const {
         return find(x) == find(y);
     }
 
-    /**
-     * @returns The size of the set containing `x`.
-    */
-    int getSize(int x) const {
-        return m_size[find(x)];
-    }
-
 private:
-    mutable std::array<int, N> m_parent;  // The parent of each element.
-    std::array<int, N> m_size;  // The size of each set, indexed by representative.
+    mutable std::array<T, N> m_parent;  // The parent of each element.
 };
 
 } // namespace SPRL
