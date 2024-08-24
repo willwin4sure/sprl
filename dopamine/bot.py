c45090ef7c3bb4a0c4d1af6fdab7b768fd116fa9
William Yue <williamy330@gmail.com>
2024-08-24 16:36:45 -0400

    refactor dopamine discord bot

--- dopamine/bot.py
+++ dopamine/bot.py
@@ -10,136 +10,152 @@ import os
 from typing import Dict, Optional, Tuple
 
 import discord
-from discord.ext import commands
+from discord import app_commands
 
-bot = commands.Bot(command_prefix="/",
-                   intents=discord.Intents.all())
 
 SPRL_PATH = os.path.dirname(os.path.realpath(__file__))
 SPRL_PATH = os.path.dirname(SPRL_PATH)
+print(f"SPRL_PATH: {SPRL_PATH}")
 
-print(SPRL_PATH)
+# Read Discord token from gitignored config file.
+with open(f"{SPRL_PATH}/dopamine/config.json") as f:
+    data = json.load(f)
+    DISCORD_TOKEN = data["DISCORD_TOKEN"]  # Sensitive data.
+    GUILD_ID = data["GUILD_ID"]
+    CHANNEL_ID = data["CHANNEL_ID"]
+
+
+intents = discord.Intents.all()
+bot = discord.Client(intents=intents)
+tree = app_commands.CommandTree(bot)
+
+
 global_progress = {}
 
-
 async def parse_all_running():
-    """
-    Parse all running jobs in the SPRL_PATH.
-    """
+    """Parse all running jobs in the SPRL_PATH."""
+    
+    progress = {}
 
-    # iterate over all files in the logs directory
-    # if they are of the form "name_<index>.log", then parse them.
-    new_progress = {}
-
-    for file in os.listdir(os.path.join(SPRL_PATH, 'logs')):
+    # Parse files of the form "name_<index>.log" in the `logs` directory.
+    for file in os.listdir(os.path.join(SPRL_PATH, "logs")):
         if not file.endswith(".log"):
             continue
+        
         name = file.rsplit(".", 1)[0]
         name = name.rsplit("_", 1)[0]
 
-        with open(os.path.join(SPRL_PATH, 'logs', file)) as f:
+        with open(os.path.join(SPRL_PATH, "logs", file)) as f:
             lines = f.readlines()
+
         iteration = 0
         for line in lines:
             if "Starting iteration" in line:
-                iteration = int(line.split()[-1][:-3])  # remove the ellipsis
+                iteration = int(line.split()[-1][:-3])  # Remove the ellipsis.
 
             if "finished training." in line:
-                # A sort of hacky way to do this.
-                iteration += 1
+                iteration += 1  # If the bot finished training.
+
         try:
             with open(f"{SPRL_PATH}/data/configs/{name}_config_selfplay.json") as f:
                 config = json.load(f)
                 total_iters = config["numIters"]
-                new_progress[name] = [iteration, total_iters]
+                progress[name] = [iteration, total_iters]
+
         except FileNotFoundError:
-            new_progress[name] = [iteration, None]
+            progress[name] = [iteration, None]
 
-    return new_progress
-
-
-# async def get_all_tournaments():
-#     """
-#     Check the robin folder and look for all
-#     """
+    return progress
 
 
 def progress_to_embed(progress: Dict[str, Tuple[int, Optional[int]]], color=discord.Color.blue()):
-    """
-    Convert the progress to a discord embed.
-    """
+    """Convert the progress to a discord embed."""
+
     embed = discord.Embed(title="Progress", color=color)
     for name, iteration in progress.items():
         if iteration[0] == 0:
             embed.add_field(name=name, value="Initializing... :arrow_forward:")
+
         if iteration[1] is not None:
             if iteration[0] >= iteration[1]:
                 embed.add_field(name=name, value="Done! :trophy:")
             else:
-                embed.add_field(name=name,
-                                value=f"Iteration {iteration[0]}/{iteration[1]}")
+                embed.add_field(name=name, value=f"Iteration {iteration[0]}/{iteration[1]}")
+
         else:
             embed.add_field(name=name, value=f"Iteration {iteration[0]}")
+
     return embed
 
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+    print(f"Logged in as {bot.user}.")
+
+    # Only sync the tree if commands have changed.
+    # await tree.sync(guild=discord.Object(id=GUILD_ID))
+
+    # Start watching the progress file.
     bot.loop.create_task(watch_file())
 
 
-@bot.command()
-async def progress(ctx: commands.Context):
-    """
-    Send the progress of all running jobs.
-    """
-    # print the channel from which the command was sent
-    print(ctx.channel.id)
+@tree.command(
+    name="progress",
+    description="Immediately send the progress of all running jobs.",
+    guild=discord.Object(id=GUILD_ID),
+)
+async def progress(interaction: discord.Interaction):
+    """Immediately send the progress of all running jobs."""
 
+    # Send the progress in an embed.
     progress = await parse_all_running()
-    # Send the progress in an embed
-    await ctx.send(embed=progress_to_embed(progress))
+    embed = progress_to_embed(progress)
+    await interaction.response.send_message(embed=embed)
 
 
-# This command should be triggered by either
-# !json or !info
-@bot.command(aliases=["json"])
-async def info(ctx: commands.Context, name: str):
-    """
-    Send the infos of all running jobs.
+@tree.command(
+    name="info",
+    description="Dumps the config files of a job.",
+    guild=discord.Object(id=GUILD_ID),
+)
+async def info(interaction: discord.Interaction, name: str):
+    """Dumps the config files of a job.
 
-    # Next, for each running job, go to data/configs
-    # and find three files:
-    #  /data/configs/{name}_config_selfplay.json
-    #  /data/configs/{name}_config_controller.json
-    #  /data/configs/{name}_config_uct.json
-    # This is a json file; you should parse it.
-    # Output each json file in a pretty embed.
+    For each running job, goes to `data/configs` and dumps:
+        /data/configs/{name}_config_selfplay.json
+        /data/configs/{name}_config_controller.json
+        /data/configs/{name}_config_uct.json
     """
 
-    # make sure name is alphanumeric with underscores.
-    flag = False
+    # Check if the name is valid.
+    invalid = False
     for c in name:
         if not c.isalnum() and c != "_":
-            flag = True
+            invalid = True
             break
-    if flag:
-        await ctx.send("Invalid name. Please use only alphanumeric characters and underscores.")
+    if invalid:
+        await interaction.response.send_message("Invalid name. Please use only alphanumeric characters and underscores.")
         return
-    config_path = f"{SPRL_PATH}/data/configs"
-    selfplay_filepath = f"{config_path}/{name}_config_selfplay.json"
-    controller_filepath = f"{config_path}/{name}_config_controller.json"
-    uct_filepath = f"{config_path}/{name}_config_uct.json"
+    
+    config_path = os.path.join(SPRL_PATH, "data", "configs")
 
-    with open(selfplay_filepath) as f:
-        selfplay: Dict = json.load(f)
+    selfplay_filepath = os.path.join(config_path, f"{name}_config_selfplay.json")
+    controller_filepath = os.path.join(config_path, f"{name}_config_controller.json")
+    uct_filepath = os.path.join(config_path, f"{name}_config_uct.json")
 
-    with open(controller_filepath) as f:
-        controller = json.load(f)
+    try:
+        with open(selfplay_filepath) as f:
+            selfplay: Dict = json.load(f)
 
-    with open(uct_filepath) as f:
-        uct = json.load(f)
+        with open(controller_filepath) as f:
+            controller: Dict = json.load(f)
+
+        with open(uct_filepath) as f:
+            uct: Dict = json.load(f)
+
+    except FileNotFoundError:
+        await interaction.response.send_message("Config files not all found.")
+        return
 
     # Send 3 separate embeds
     selfplay_embed = discord.Embed(title=f"{name}_config_selfplay.json")
@@ -154,19 +170,16 @@ async def info(ctx: commands.Context, name: str):
     for key in uct.keys():
         uct_embed.add_field(name=key, value=uct[key])
 
-    await ctx.send(embed=selfplay_embed)
-    await ctx.send(embed=controller_embed)
-    await ctx.send(embed=uct_embed)
+    await interaction.response.send_message(embeds=[selfplay_embed, controller_embed, uct_embed])
 
 
 async def watch_file():
-    """
-    Watch the progress file and send it to the channel.
-    """
+    """Watch the progress file and send it to the channel."""
+
     global global_progress
-    channel = bot.get_channel(1259883219046764615)
-    # int(os.getenv("CHANNEL_ID")))
+    channel = bot.get_channel(CHANNEL_ID)
     while True:
+        print("Checking progress.")
         # Check the progress. If it is different from before, send it to the channel.
         # Edge case: if a process existed before but does not anymore, we do not send anything!
         progress = await parse_all_running()
@@ -177,16 +190,13 @@ async def watch_file():
                 changed = True
                 if iteration[1] is not None and iteration[0] >= iteration[1]:
                     completed_tasks.append(name)
+
         global_progress = progress
         if changed:
             await channel.send(embed=progress_to_embed(progress))
 
-        # Sleep for 5 minutes (usually, changed is False)
+        # Sleep for 5 minutes (usually, changed is False).
         await asyncio.sleep(300)
 
 
-# read discord token from a json file
-with open(f"{SPRL_PATH}/dopamine/config.json") as f:
-    data = json.load(f)
-    DISCORD_TOKEN = data["DISCORD_TOKEN"]
 bot.run(DISCORD_TOKEN)
